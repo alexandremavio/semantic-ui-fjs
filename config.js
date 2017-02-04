2e681ff32ba84a823fb6edad007928ccd6f36b7c
ferrugemjs <ferrugemjavascript@gmail.com>
2017-02-04 22:11:19 +0000

    update typescript,ferrugemjs versions

--- config.js
+++ config.js
@@ -16,6 +16,7 @@ System.config({
     "incremental-dom": "npm:incremental-dom@0.5.1",
     "semantic-ui-fjs": "dist/ui/index",
     "ts-helpers": "npm:ts-helpers@1.1.1",
+    "tslib": "npm:tslib@1.5.0",
     "github:jspm/nodelibs-assert@0.1.0": {
       "assert": "npm:assert@1.4.1"
     },
